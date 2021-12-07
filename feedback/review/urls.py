bee0fabd241898b23b77498a2273c94b748f7268
shivshankar31 <shivshankar31@yahoo.com>
2021-12-07 05:55:22 +0530

    136: step 5: handling form subimission,   redirect after submitting

--- feedback/review/urls.py
+++ feedback/review/urls.py
@@ -1,8 +1,13 @@
 # step 1.4: In urls.py, import path, views and create the urlpatterns.
+# step 5.6: include PATH in urls.py and call the thankyou function.
+
+
+
 
 from django.urls import path
 from .import views 
 
 urlpatterns = [
-    path("", views.review )
+    path("", views.review ),
+    path('thankyou', views.thankyou )
 ]
--- feedback/review/views.py
+++ feedback/review/views.py
@@ -1,12 +1,25 @@
 # step 1.3: define a review in views.py file.
 # Step 2.2: add the created html file to views.py to render
+# step 5.3: as we get GET and POST in the same url, we need to identify using IF statment and redirect to thankyou.html, we can also render inside if statment 
+#           but it is not a good pratice. create a seperate function for thankyou and render the html page there.
+# step 5.4: inclding HttpResponceRedirect we can add the extension url over there.
 
 
-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def review(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        print(username)
+        return HttpResponseRedirect("/thankyou")
+
     return render (request, "review/review.html")
+
+
+
+def thankyou(request):
+    return render(request, "review/thankyou.html")
