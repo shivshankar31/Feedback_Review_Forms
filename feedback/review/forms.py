0a0ddffa7f9b8ec63d08a2601bbc294384fbb0ed
shivshankar31 <shivshankar31@yahoo.com>
2021-12-08 10:24:05 +0530

    145: step 13:  ModelForm - Auto generate form from models

--- feedback/review/forms.py
+++ feedback/review/forms.py
@@ -10,15 +10,22 @@ from typing import Text
 from django import forms
 from django.forms.forms import Form
 from django.forms.widgets import TextInput
+from .models import Review
 
 
 
 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your Name", max_length=20, required= True, error_messages={
-        'required' : 'This field must not be empty',
-        'max_length': 'Length of your name should not be more than 20'
-    })
-    feedback = forms.CharField(label = 'Your Review', widget = forms.Textarea, max_length= 200)
-    rating = forms.IntegerField(min_value=1, max_value=5)
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name", max_length=20, required= True, error_messages={
+#         'required' : 'This field must not be empty',
+#         'max_length': 'Length of your name should not be more than 20'
+#     })
+#     feedback = forms.CharField(label = 'Your Review', widget = forms.Textarea, max_length= 200)
+#     rating = forms.IntegerField(min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    
+    class Meta:
+        model = Review
+        fields = '__all__'
