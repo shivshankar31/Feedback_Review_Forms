e2db08591ed91aa3ea97b658d3a50e028ce09e49
shivshankar31 <shivshankar31@yahoo.com>
2021-12-08 05:54:37 +0530

    143:  step 11: adding more fields

--- feedback/review/forms.py
+++ feedback/review/forms.py
@@ -1,13 +1,15 @@
 # step 6.1: create forms.py file and import forms form django, to handle forms and validation automaticly
 # step:8.1: in forms.py, we can chage the lable, max_length, required(*)- by default it set to be true, error_massages
+# step 11.1: in forms.py, add feedbask field and rating field.
 
 
 
 
 
-
+from typing import Text
 from django import forms
 from django.forms.forms import Form
+from django.forms.widgets import TextInput
 
 
 
@@ -18,3 +20,5 @@ class ReviewForm(forms.Form):
         'required' : 'This field must not be empty',
         'max_length': 'Length of your name should not be more than 20'
     })
+    feedback = forms.CharField(label = 'Your Review', widget = forms.Textarea, max_length= 200)
+    rating = forms.IntegerField(min_value=1, max_value=5)
