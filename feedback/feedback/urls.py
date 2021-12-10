f12d8e72f9cc0dc7814b7e9fe2360e9c6ebe3912
shivshankar31 <shivshankar31@yahoo.com>
2021-12-11 03:51:26 +0530

    Step 1.6: url path included

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -18,10 +18,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('review.urls'))
+    path('', include('review.urls')),
+    path('profiles/', include('profiles.urls'))
 ]
